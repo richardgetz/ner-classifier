d25c88cfc45eac37236f17be95396904fe50031f
Richard Getz <richardgetziii@gmail.com>
2020-06-30 16:03:08 -0400

    adding changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import json
 import webbrowser
 from werkzeug.utils import secure_filename
@@ -8,28 +8,193 @@ import sys
 import shutil
 import re
 import uuid
-
+import glob
+import spacy
+import pkg_resources
+# from data_controller import save boundaries
 app = Flask(__name__)
 app.secret_key = str(uuid.uuid1())
+DATASET_LOADED = False
+ACTIVE_LEARNING_MODEL = None
+# TODO: add as default config instead and make editable in the frontend
+LABELS = ["PERSON", "LOC", "ORG", "GPE", "NORP", "PHONE", "EMAIL"]
+DATA_STORE_LOCATION = ""
 domain = "http://127.0.0.1"
+CERT_LOCATION = "adhoc"
 port = 5000
+pkgs = pkg_resources.working_set
+pkgs_1 = sorted(["%s==%s" % (i.key, i.version) for i in pkgs])
+ALL_INSTALLED = re.findall(
+    r"([a-z]{2,3}(?:\-\w+){2,5}\-(?:sm|md|lg))", str(pkgs_1))
+ACTIVE = []
+AWAIT_OVERRIDE = False
+# this would be built from spacy' output
+DATASET = [{"uuid": 0,
+            "sentence": [{"id": 0, "word": "John"}, {"id": 1, "word": "Smith"}, {"id": 2, "word": "is"}, {"id": 3, "word": "the"}, {"id": 4, "word": "user"},
+                         {"id": 5, "word": "of"}, {"id": 6, "word": "john@mail.com"}],
+            "spans": {"PERSON": ["0:1"], "EMAIL":["6:6"]}},
+           {"uuid": 1,
+            "sentence": [{"id": 0, "word": "Here"}, {"id": 1, "word": "is"}, {"id": 2, "word": "a"}, {"id": 3, "word": "sentence"}, {"id": 4, "word": "without"},
+                         {"id": 5, "word": "any"}, {"id": 6, "word": "tags"}],
+            "spans": None}]
+ALPHA_DATASET = []
+ALLOWED_EXTENSIONS = ['json']
 
-tokens = [{"id": 0, "word": "This"}, {"id": 1, "word": "is"}, {"id": 2, "word": "an"}, {"id": 3, "word": "example"},
-          {"id": 4, "word": "of"}, {"id": 5, "word": "what"},
-          {"id": 6, "word": "a"}, {"id": 7, "word": "sentence"},
-          {"id": 8, "word": "looks"}, {"id": 9, "word": "like"}, {"id": 10, "word": "."}]
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/train', methods=['GET', 'POST'])
+def train():
+    if request.method == 'GET':
+        return "This will eventually allow you to train with a few clicks. Not worried about this yes since we have to test training data first."
+        return render_template('train.html')
+    if request.method == 'POST':
+        print("in progress")
+
+
+@app.route('/dataset_loader', methods=['GET', 'POST'])
+def load_data():
+
+    global ALL_INSTALLED
+    models = ALL_INSTALLED
+    print(models)
+    global DATA_STORE_LOCATION
+    if request.method == 'GET':
+        if os.path.exists(os.path.join(DATA_STORE_LOCATION, "completed_models")):
+            for file in os.listdir(os.path.join(DATA_STORE_LOCATION, "completed_models")):
+                models.append(str(file))
+        if len(models) == 0:
+            models = None
+        if os.path.exists(os.path.join(DATA_STORE_LOCATION, "datasets/untrained")):
+            datasets = []
+            for file in os.listdir(os.path.join(DATA_STORE_LOCATION, "datasets/untrained")):
+                if file.endswith(".json"):
+                    datasets.append({"label": str(file).rstrip(
+                        ".json"), "filename": str(file)})
+
+            if datasets == []:
+                datasets = None
+            return render_template('load_generate.html', datasets=datasets,
+                                   models=models)
+
+        else:
+            return render_template('load_generate.html', datasets=None,
+                                   models=models)
+
+    if request.method == 'POST':
+        req = request.form.to_dict()
+        if not os.path.exists(DATA_STORE_LOCATION):
+            os.mkdir(DATA_STORE_LOCATION)
+        if not os.path.exists(os.path.join(DATA_STORE_LOCATION, "datasets/untrained")):
+            os.mkdir(os.path.join(DATA_STORE_LOCATION, "datasets/untrained"))
+
+        file = request.files["file"]
+        max = None
+        try:
+            if req.get("max-sentences", False):
+                max = int(req["max-sentences"])
+        except:
+            pass
+        use_dataset = None
+        if file:
+            if allowed_file(file.filename):
+                if req.get("name", False):
+                    ext = str(file.filename).split(".")[-1]
+                    filename = secure_filename(req["name"] + "." + ext)
+                else:
+                    filename = secure_filename(file.filename)
+                use_dataset = filename
+                full_path = os.path.join(os.path.join(DATA_STORE_LOCATION,
+                                                      "datasets/untrained"), filename)
+                file.save(full_path)
+
+        print("in progress")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        return render_template('index.html', tokens=tokens)
-    if request.method == 'POST':
 
-        return render_template('index.html')
+    global LABELS
+    global DATASET
+    global DATASET_LOADED
+    global ACTIVE
+    global AWATT_OVERRIDE
+    if len(DATASET) > 0:
+        DATASET_LOADED = True
+    if request.method == 'GET':
+        if not DATASET_LOADED:
+            flash("No active data to train on. Please load some data first.")
+            return redirect(url_for('load_data'))
+        if len(DATASET) == 0:
+            if len(ACTIVE) == 0:
+                flash(
+                    "No data left to train on. Either load more or cheer because you're done!")
+                return redirect(url_for('load_data'))
+        elif len(ACTIVE) > 0:
+            if AWAIT_OVERRIDE:
+                AWAIT_OVERRIDE = False
+                return render_template('index.html', labels=LABELS,
+                                       tokens=ACTIVE[0]["sentence"], spans=ACTIVE[0]["spans"],
+                                       sentence_id=ACTIVE[0]["uuid"])
+
+            else:
+                AWAIT_OVERRIDE = True
+                flash("Someone is still training on your current dataset. Highly recommend waiting until they are finished. If you go to the homepage you will forceably take their training task.")
+                return redirect(url_for('load_data'))
+        else:
+            ACTIVE.append(DATASET[0])
+            send = DATASET[0]
+            DATASET.remove(DATASET[0])
+            return render_template('index.html', labels=LABELS,
+                                   tokens=send["sentence"], spans=send["spans"], sentence_id=send["uuid"])
+    if request.method == 'POST':
+        req = request.form.to_dict()
+        sentence_id = None
+        sentence_tags = {}
+        for a in ACTIVE:
+            if str(a["uuid"]) == req["sentence_id"]:
+                sentence_id = req["sentence_id"]
+                ACTIVE.remove(a)
+                break
+        if sentence_id:
+            for tag in req.keys():
+                if tag == "sentence_id":
+                    continue
+
+                sentence_tags[tag] = req[tag].split(",")
+
+        print(sentence_id, sentence_tags)
+        if not DATASET_LOADED:
+            flash(
+                "No data left to train on. Either load more or cheer because you're done!")
+            return redirect(url_for('load_data'))
+        available = False
+        while not available:
+            if len(DATASET) < 1:
+                if len(ACTIVE) == 0:
+                    DATASET_LOADED = False
+                    flash(
+                        "No data left to train on. Either load more or cheer because you're done!")
+                    return redirect(url_for('load_data'))
+                elif len(ACTIVE) > 0:
+                    if AWAIT_OVERRIDE:
+                        AWAIT_OVERRIDE = False
+                        return render_template('index.html', labels=LABELS, tokens=ACTIVE[0]["sentence"], spans=ACTIVE[0]["spans"], sentence_id=ACTIVE[0]["uuid"])
+
+                    else:
+                        AWAIT_OVERRIDE = True
+                        return "There is no more data to show you but another user is still tagging a sentence. If you reload this page we will forceably give you the sentence."
+            else:
+                ACTIVE.append(DATASET[0])
+                send = DATASET[0]
+                DATASET.remove(DATASET[0])
+                return render_template('index.html', tokens=send["sentence"], sentence_id=send["uuid"], labels=LABELS,
+                                       spans=send["spans"])
 
 
 if __name__ == '__main__':
     webbrowser.open(domain + ":" + str(port))
-    # change to ssl on prod
     app.run(debug=False, port=port, threaded=True)
--- static/js/tagManager.js
+++ static/js/tagManager.js
@@ -1,34 +1,78 @@
 var t = '';
 var button_label = 'PERSON';
 var last = null;
-window.addEventListener("mouseup", gText);
+var tag_dictionary = {};
+var sentence_id = null;
+// window.addEventList("mouseup", gText);
+
+function listenReset() {
+  window.addEventListener("mouseup", gText);
+  t = '';
+}
+listenReset();
+
+function post() {
+  url = "/";
+  method = "post";
+  // Create the form object
+  var form = document.createElement("form");
+  form.setAttribute("method", method);
+  form.setAttribute("action", url);
+  const hiddenField = document.createElement("input");
+  hiddenField.setAttribute("type", "hidden");
+  hiddenField.setAttribute("name", "sentence_id");
+  hiddenField.setAttribute("value", sentence_id);
+  form.appendChild(hiddenField);
+  // For each key-value pair
+  for (key in tag_dictionary) {
+    const hiddenField = document.createElement("input");
+    hiddenField.setAttribute("type", "hidden");
+    hiddenField.setAttribute("name", key);
+    hiddenField.setAttribute("value", tag_dictionary[key]);
+    form.appendChild(hiddenField);
+  }
+  document.body.appendChild(form); // inject the form object into the body section
+  form.submit();
+}
+
 
 function removeTag(elementid) {
   var button = document.getElementById(elementid);
   var sentence = document.getElementById("sentence");
-  // console.log(element);
-  var label = button.parentNode;
-  var highlight = label.parentNode;
-  console.log(sentence.childNodes.length);
-  console.log(highlight.childNodes.length);
-  // I need to find a way to handle the count and figure out when it is the last toke to use append instead of insertBefore
-  var k = 0;
-  for (k += parseInt(highlight.childNodes.length) - 1; k > -1; k--) {
-    console.log(k);
-    console.log(highlight.childNodes[k]);
-    console.log(highlight.childNodes[k].name);
-    var i;
-    for (i = 0; i < sentence.childNodes.length; i++) {
-      if (parseInt(sentence.childNodes[i].id) == (parseInt(highlight.childNodes[k].id) + 1)) {
-        console.log(sentence.childNodes[i].id);
-        sentence.insertBefore(highlight.childNodes[k], sentence.childNodes[i]);
-        break;
+  var highlight = button.parentNode;
+  var start = null;
+  var end = null;
+  var label_type = null;
+  for (var m = 0; m < highlight.childNodes.length; m++) {
+    if (highlight.childNodes[m].id == "label") {
+      label_type = String(highlight.childNodes[m].textContent);
+      label_remove = highlight.childNodes[m];
+    } else if (highlight.childNodes[m].id != null && highlight.childNodes[m].id != "") {
+      if (start == null) {
+        start = parseInt(highlight.childNodes[m].id)
+        end = parseInt(highlight.childNodes[m].id)
+      }
+      if (parseInt(highlight.childNodes[m].id) > end) {
+        end = parseInt(highlight.childNodes[m].id)
       }
     }
   }
-  label.remove();
-  highlight.remove();
-  button.remove();
+  console.log("removing", end)
+  $(document).ready(function() {
+    $("#label_highlight_" + String(end) + " span").unwrap();
+  });
+  try {
+    label_remove.remove();
+    button.remove();
+    // highlight.remove();
+  } catch (e) {}
+  try {
+    var index_rem = tag_dictionary[label_type].indexOf(start + ":" + end);
+    if (index_rem > -1) {
+      tag_dictionary[label_type].splice(index_rem, 1);
+    }
+  } catch (e) {}
+  console.log(tag_dictionary);
 }
 
 function getLabel() {
@@ -41,8 +85,46 @@ function getLabel() {
   }
 }
 
+function initialLabels() {
+  for (var key in tag_dictionary) {
+    var local_label = key;
+    for (var val in tag_dictionary[key]) {
+      var last = null;
+      var i = null;
+      var a = null;
+      var create_wrapper = '';
+      i = parseInt(tag_dictionary[key][val].split(":")[0]);
+      a = parseInt(tag_dictionary[key][val].split(":")[1]);
+      console.log(i, a)
+      if (i == a) {
+        create_wrapper = "#" + String(i);
+        last = i;
+        $(create_wrapper).wrap('<mark class="label_highlight" id="label_highlight_' + String(last) + '"></mark>');
+      } else {
+        while (i <= a) {
+          create_wrapper += "#" + String(i) + ", ";
+          last = i;
+          i++;
+        }
+        create_wrapper = create_wrapper.replace(/,\s$/g, '');
+        $(create_wrapper).wrapAll('<mark class="label_highlight" id="label_highlight_' + String(last) + '"></mark>');
+      }
+      var good_selection = false;
+      if (i != null && a != null && i != "NaN" && a != "NaN") {
+        good_selection = true;
+      }
+      if (last != null && good_selection == true) {
+        $('<span class="label_text" id="label">' + String(local_label) +
+          '</span><span class="xbutton" id="after_' + String(last) + '_button" onclick="removeTag(this.id)">x</span>').insertAfter('#' + String(last));
+      }
+    }
+  }
+}
+
 function gText() {
   getLabel();
+  var start = null;
+  var end = null;
   var t = window.getSelection();
   if (!t.isCollapsed) {
     var n = t.getRangeAt(0),
@@ -52,22 +134,42 @@ function gText() {
     var create_wrapper = '';
     if (i == a) {
       create_wrapper = "#" + String(i);
-      $(create_wrapper).wrap('<mark class="label_highlight"></mark>');
       last = i;
+      $(create_wrapper).wrap('<mark id="label_highlight_' + String(last) + '" class="label_highlight"></mark>');
+      start = i;
+      end = i;
     } else {
-
+      start = i;
       while (i <= a) {
         create_wrapper += "#" + String(i) + ", ";
         last = i;
         i++;
       }
+      end = i - 1;
       create_wrapper = create_wrapper.replace(/,\s$/g, '');
-      $(create_wrapper).wrapAll('<mark class="label_highlight"></mark>');
+      $(create_wrapper).wrapAll('<mark id="label_highlight_' + String(last) + '" class="label_highlight"></mark>');
     }
-    if (last != null) {
-      $('<span class="label_text">' + String(button_label) + '<span class="xbutton" id="after_' + String(last) + '_button" onclick="removeTag(this.id)">×</span></span>').insertAfter('#' + String(last));
+    var good_selection = false;
+    if (start != null && end != null && start != NaN && end != NaN && start + ":" + end != "NaN:NaN" && end >= start) {
+      good_selection = true;
+      var exists = false;
+      for (var key in tag_dictionary) {
+        if (key == button_label) {
+          exists = true;
+          break;
+        }
+      }
+      if (exists == true) {
+        tag_dictionary[button_label].push(start + ":" + end);
+      } else {
+        tag_dictionary[button_label] = [start + ":" + end];
+      }
+    }
+    if (last != null && good_selection == true) {
+      $('<span class="label_text" id="label">' + String(button_label) +
+        '</span><span class="xbutton" id="after_' + String(last) + '_button" onclick="removeTag(this.id)">x</span>').insertAfter('#' + String(last));
     }
   }
-  // this shouldnt be necessary but on some browsers it marks the wrong content
-  t = null;
+  console.log(tag_dictionary);
+  listenReset();
 }
